ed0f901ea508917096120a1483726b8d28d611ed
etnik2002 <46058633+etnik2002@users.noreply.github.com>
2023-05-29 21:43:51 +0200

    test 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function etnik() {
-    console.log("etnik zeqiri")
+const axios = require("axios");
+
+async function sulmojNeBackend(url, data) {
+    await axios.post(url, data).then((res) => console.log(res.data));
 }
 
+async function hollojPiBackEndi(url) {
+    await axios.get(url).then((res) => console.log(res.data)); 
+    
+}
 
-
-module.exports = etnik;
+module.exports = { sulmojNeBackend, hollojPiBackEndi };
